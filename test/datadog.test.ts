92bfb42332fd5b80133175990cf1e2a09a72f025
Paul Grenyer <paul.grenyer@gmail.com>
2022-06-22 12:49:38 +0100

    feat: added extra test for some more coverage.

--- test/datadog.test.ts
+++ test/datadog.test.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const apiKey = process.env.API_KEY ?? '';
 
 describe('Transport Datadog', () => {
-    it('Smoke test', () => {
+    it('Smoke test - with tags', () => {
         const logger = new Logger({
             processors: [datadogProcessor()],
             transports: [
@@ -31,5 +31,27 @@ describe('Transport Datadog', () => {
         logger.info('(INFO) Retrieved book', { book });
         logger.warn('(WARN) Retrieved book', { book });
         logger.error('(ERROR) Retrieved book');
+    });
+
+    it('Smoke test - without tags', () => {
+        const logger = new Logger({
+            processors: [datadogProcessor()],
+            transports: [
+                datadogTransport({
+                    apiKey: apiKey,
+                    hostname: 'hostname',
+                    service: 'service',
+                    ddsource: 'nodejs',
+                    intakeRegion: 'eu',
+                }),
+                transports.stream(),
+            ],
+        });
+        const book = {
+            title: 'Monkey',
+            author: "Wu Ch'eng-en",
+            ISBN10: '9780140441116',
+        };
+        logger.info('(INFO) Retrieved book', { book });
     });
 });
