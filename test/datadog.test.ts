2c3f1cfb9a0153d2ff89f2d68a8e04fc0bc9b292
Paul Grenyer <paul.grenyer@gmail.com>
2024-06-15 07:07:45 +0100

    fix: fixed context

--- test/datadog.test.ts
+++ test/datadog.test.ts
@@ -1,16 +1,32 @@
-import { Level, Logger, transports } from 'tripitaka';
+import { AsyncLocalStorage } from 'async_hooks';
+import { Level, Logger, processors, transports } from 'tripitaka';
 import { datadogTransport } from '../src/transports/datadog-transport';
 import { datadogProcessor } from '../src/processors/datadog-processor';
 import dotenv from 'dotenv';
 dotenv.config();
 
+const { context, timestamp, augment } = processors;
+
 const apiKey = process.env.API_KEY ?? '';
 
+export const als = new AsyncLocalStorage<Map<string, any>>();
+
+const localStorage = augment({
+    source: () => {
+        const store = als.getStore();
+
+        /* istanbul ignore next */
+        if (store) {
+            return Object.fromEntries(store);
+        }
+    },
+});
+
 describe('Transport Datadog', () => {
-    it('Smoke test - with tags', () => {
+    it.only('Smoke test - with tags', () => {
         const logger = new Logger({
             level: Level.TRACE,
-            processors: [datadogProcessor()],
+            processors: [localStorage, context(), timestamp(), datadogProcessor()],
             transports: [
                 datadogTransport({
                     apiKey: apiKey,
@@ -28,11 +44,15 @@ describe('Transport Datadog', () => {
             author: "Wu Ch'eng-en",
             ISBN10: '9780140441116',
         };
-        logger.trace('(TRACE) Retrieved book', { book });
-        logger.debug('(DEBUG) Retrieved book', { book });
-        logger.info('(INFO) Retrieved book', { book });
-        logger.warn('(WARN) Retrieved book', { book });
-        logger.error('(ERROR) Retrieved book');
+
+        als.run(new Map(), () => {
+            als.getStore()?.set('version', '1.0.0');
+            logger.trace('(TRACE) Retrieved book', { book });
+            logger.debug('(DEBUG) Retrieved book', { book });
+            logger.info('(INFO) Retrieved book', { book });
+            logger.warn('(WARN) Retrieved book', { book });
+            logger.error('(ERROR) Retrieved book');
+        });
     });
 
     it('Smoke test - without tags', () => {
